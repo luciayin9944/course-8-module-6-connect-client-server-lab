c532c9bc86be46f795eab7b87eeec9f3cdcfb59f
luciayin9944 <luxiayin94@gmail.com>
2025-06-13 14:46:22 -0700

    update client&server

--- client/script.js
+++ client/script.js
@@ -1,26 +1,34 @@
-//display events list
-fetch("/events")
-  .then((res) => res.json())
-  .then((events) => {
-    events.forEach(event => {
-      const item = document.createElement("li");
-      item.textContent = `${id}: ${title}`;
-      document.getElementById("event-list").append(item);
+//fetch data
+document.getElementById('fetch-events').addEventListener("click", () =>{
+  // fetch('/events')  // Relative Path
+  fetch('http://localhost:5000/events') //Absolute Path
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(event => renderEvent(event))
     });
-  });
+});
 
+// add event
 document.getElementById("event-form").addEventListener("submit", (e) => {
   e.preventDefault();
-  const input = document.querySelector("input").value;
-  fetch("/events", {
+  const input = document.getElementById("event-title").value;
+  // fetch("/events", {
+  fetch('http://localhost:5000/events', {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ title: input })
   })
   .then(res => res.json())
-  .then(event => {
-    const item = document.createElement("li");
-    item.textContent = `${id}: ${title}`;
-    document.getElementById("event-list").append(item);
+  .then(event => { 
+    renderEvent(event)
   });
 });
+
+
+// render list
+const renderEvent = (event) => {
+  const list = document.getElementById("events-list");
+  const item = document.createElement("li");
+  item.textContent = event.title;
+  list.appendChild(item)
+}
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Create a list called 'events' with a couple of sample event dictionaries
 # Each dictionary should have an 'id' and a 'title'
@@ -13,13 +15,14 @@ events = [
 # This route should return a JSON welcome message
 @app.route("/")
 def home():
-    return '<h1>Welcome!</h1>'
+    #return '<h1>Welcome!</h1>'
+    return jsonify({"message": "Welcome!"}), 200
 
 # TASK: Create a GET route for "/events"
 # This route should return the full list of events as JSON
 @app.route("/events", methods=["GET"])
 def get_events():
-    return jsonify(events)
+    return jsonify(events), 200 ##return a object
 
 # TASK: Create a POST route for "/events"
 # This route should:
@@ -31,10 +34,15 @@ def get_events():
 @app.route("/events", methods=["POST"])
 def add_event():
     data = request.get_json()
+    title = data.get('title')
+    if not title:
+        return jsonify({"message": "Title must be provide."}), 400
     new_id = max((e["id"] for e in events), default=0) + 1
     new_event = {"id": new_id, "title": data["title"]}
     events.append(new_event)
-    return jsonify(new_event), 201
+    return jsonify(new_event), 201 ##return a object
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
